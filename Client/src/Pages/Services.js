fbc7ddd82b786b87afb955de26cf4ebdfc9202de
hariharan610 <hariganesh610@gmail.com>
2023-09-18 23:14:46 +0530

    frontend commit - till linking

---
+++ Client/src/Pages/Services.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Services(){
+    return( 
+        <div>
+            <Navbar></Navbar>
+            <div><h1>Services Page</h1></div>
+        </div>
+    )
+}
+
+export default Services
