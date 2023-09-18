d16c96874cd9f5a6a45b8580b2b610ff3f809575
AshishRam7 <jaashishram7@gmail.com>
2023-09-19 00:17:39 +0530

    Updated the API directory with the Matlab parser

---
+++ Server/API/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const matlabf = require('./Matlab files/data/matlabData.json');
+
+app.get("/api1", (req, res) => {
+    try{
+        res.json(matlabf);
+    }
+    catch(err){
+        console.log(err);
+    }
+
+});
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'app', 'index.html'));
+});
+
+app.get("/api", (req, res) => {
+    res.json({"users":["user1","user2","user3", "server9"]});
+});
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
