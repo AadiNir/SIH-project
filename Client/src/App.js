70fd73fdf3b51e7aaa301727c71de0dcaaaa93ae
AshishRam7 <jaashishram7@gmail.com>
2023-09-19 14:09:37 +0530

    Hero Slider and their images added
    I added the hero slider component in the app.js , the text on top of it(should be aligned properly , informed Hari)
    
    the buttons sometimes render on the same side , needs fixing

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
+import AgroTrucker from './AgroTrucker.jfif';
+import AgroTrucker2 from './AgroTrucker2.jfif';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
 function App() {
-    return(
-        <React.Fragment>
-            <Navbar/>
-        </React.Fragment>
-    );
+  const slides = [AgroTrucker, AgroTrucker2];
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className='relative'>
+        <div style={{ top: '10px', left: '2000px' }}>
+          <h1 className='absolute text-white text-4xl'>
+            Welcome<br />to <b>AgroChain!</b>
+          </h1>
+          <h2 className='fixed text-white text-1xl'>
+            Smart Fruit Health Monitoring<br/>and Tracking System
+          </h2>
+        </div>
+
+          <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactLeft onClick={prevSlide} />
+          </div>
+          <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactRight onClick={nextSlide} />
+          </div>
+        <img
+          className='w-screen h-screen object-cover top-16'
+          src={slides[currentSlide]}
+          alt={`Slide ${currentSlide + 1}`}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
