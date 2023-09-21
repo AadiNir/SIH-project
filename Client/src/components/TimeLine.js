8bf1329a0308d2deb3744638fb3e021272fc9398
Aadinir <aadiniranjan27@gmail.com>
2023-09-21 22:42:34 +0530

    ui-for-timeline

--- Client/src/components/TimeLine.js
+++ Client/src/components/TimeLine.js
@@ -57,12 +57,19 @@ function TimeLine() {
     }
   }
   return (
-    <div>
-      <input type="number" placeholder="enter the product id" onChange={e => setsearchid(e.target.value)} />
-      <button onClick={get}>Search for the product</button>
-
-      <h1>Timeline of {prodname}</h1>
-      <h1>Owner: {owner}</h1>
+    <div className="timeline">
+    <div class='h-screen'>
+    <div class="flex items-center justify-center relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            
+        </div>
+      <input type="number" placeholder="enter the product id"  class="block  p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-1/3" onChange={e => setsearchid(e.target.value)} />
+      <button onClick={get} class="text-white absolute  items-center h-12 top-1 right-96 w-44 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search for the product</button>
+    </div>
+      <h1 class="flex items-center justify-center py-5 font-mono text-orange-50 font-bold text-xl">Timeline of {prodname}</h1>
+      <h1 class="flex items-center justify-center py-5 font-mono text-orange-50 font-bold text-xl	">Owner: {owner}</h1>
+ 
+      
 
       <VerticalTimeline>
         {pendingaddress.map((data, index) => (
@@ -83,6 +90,7 @@ function TimeLine() {
       </VerticalTimeline>
       <Modal open={bool} onclose={()=>setbool(false)} value={"No Such order has been initiated yet, Try again"}/>
 
+    </div>
     </div>
   );
 }
